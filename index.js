f4c88c546aeffd901180f806d05ceceb5a3f04cc
Chris Behan <chrisbehan1997@gmail.com>
2019-04-06 17:02:27 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// root route path
+app.get('/', (req, res) => res.send('This is root'))
+
+// hello route path
+app.get('/hello', (req, res) => res.send('Hello SENG 513'))
+
+// ? : The endpoint must have 0 or 1 of the preceding character.
+// E.g. a route path of '/ab?cd' will match endpoints acd or abcd.
+app.get('/abcd?word', function (req, res) {
+  res.send('word')
+})
+
+app.get('/ab(cd)?planorama', function (req, res) {
+  res.send('ab(cd)?planorama')
+})
+// * represents any string
+app.get('/planorama*ever', function (req, res) {
+  res.send('planorama*ever')
+})
+// enclose in / / to use regular expressions
+// This endpoint will match any string with the word "dog"
+app.get(/dog/, function (req, res) {
+  res.send('dog')
+})
+
+app.get('/students/:ucid/grades/:classid', function (req, res) {
+  res.send(req.params)
+})
+
+// Can have multiple callback functions
+app.get('/multiple', function (req, res, next) {
+	console.log('First callback')
+  next()
+}, function (req, res) {
+	console.log('Second callback')
+  res.send('Response from second callback')
+})
+
+var cb1 = function (req, res, next) {
+  console.log('first')
+  next()
+}
+
+var cb2 = function (req, res, next) {
+  console.log('second')
+  next()
+}
+
+var cb3 = function (req, res) {
+	console.log('third')
+  res.send('Hello from the third callback')
+}
+
+// can set the second parameter as an array of callback functions
+app.get('/3callbacks', [cb1, cb2, cb3])
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
